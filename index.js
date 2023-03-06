c34ebac8768dd36288bbe78c50c4bb4265228dbc
Thomas Wallingford <tbwallingford@gmail.com>
2023-03-06 03:30:03 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let frontHalf = 0;
+  let backHalf = word.length - 1;
+
+  for(const char in word){
+    if(word[frontHalf] != word[backHalf]){
+      return false
+    }
+    frontHalf++;
+    backHalf--;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+For this version, I sort of followed the video, but wanted to challenge myself
+to see if I could use the same problem explanation and make it slightly different.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +27,41 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("tobot"));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("shallicomparetheetoasummersday"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mrowlatemymetalworm"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("wasitacaroracatisaw"));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("tortilla"));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("apples"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("eevee"));
 }
 
 module.exports = isPalindrome;
